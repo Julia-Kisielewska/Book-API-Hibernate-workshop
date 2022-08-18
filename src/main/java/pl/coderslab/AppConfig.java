fadb711e8fea24e5e383c0c26a82837d45dfc407
julia <julia.kisielewska@gmail.com>
2022-08-18 09:51:47 +0200

    20220818_init

--- src/main/java/pl/coderslab/AppConfig.java
+++ src/main/java/pl/coderslab/AppConfig.java
@@ -4,17 +4,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import pl.coderslab.model.MockBookService;
 
+import javax.persistence.EntityManagerFactory;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "pl.coderslab")
+@EnableTransactionManagement
+@EnableJpaRepositories
+
 public class AppConfig implements WebMvcConfigurer {
 
     @Bean
     @Primary
     public MockBookService mockBookService(){return new MockBookService();}
+
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
+        emfb.setPersistenceUnitName("workshopHibernate");
+        return emfb;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
 
 }
--- src/main/java/pl/coderslab/model/Book.java
+++ src/main/java/pl/coderslab/model/Book.java
@@ -2,14 +2,20 @@ package pl.coderslab.model;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Data
 @AllArgsConstructor
 @ToString
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
+@Table(name = "books")
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String isbn;
     private String title;
